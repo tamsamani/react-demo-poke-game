f42b45e924d0ca6ac2439ef64000a325c54e6acb
tamsamani <tamsmoha@gmail.com>
2020-04-20 05:14:42 +0000

    3 files modified, 1 added, 2 deleted

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-import './style.scss';
+import './styles/index.scss';
 
 function App () {
   
@@ -9,11 +9,11 @@ function App () {
       <div>
         <h1> Pokemon Game </h1>
         <p>
-          Start editing to see some magic happen :)
+          Start editing to make a magic game :)
         </p>
       </div>
     );
-  }
+
 }
 
 render(<App />, document.getElementById('root'));
