c160ab078be3f571b63a0332af55cf7e75126c61
tamsamani <tamsmoha@gmail.com>
2020-05-28 17:39:00 +0000

    normalizing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import Scene from "../Components/Scene";
+// import Scene from "../Components/Scene";
 
 export default function () {
 	return (
 		<div>
 			<h1> Pokemon Game </h1>
 			<p>Start editing to make a magic game :)</p>
-			<Scene>This is Scene</Scene>
 		</div>
 	);
+			// <Scene>This is Scene</Scene>
 }
